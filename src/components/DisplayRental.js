6eaeda93fd2001225d46a3948c780141448ec0ec
TylerT <alpinetyler@gmail.com>
2019-06-02 14:02:04 -0600

    working commit

--- src/components/DisplayRental.js
+++ src/components/DisplayRental.js
@@ -8,18 +8,31 @@ const formatter = new Intl.NumberFormat('en-US', {
     minimumFractionDigits: 2
 })
 
+//display number with thousands separator
+function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+  }
+
 export default class DisplayRental extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            edit: false
+            edit: false,
+            display: false
         }
     }
     
     toggleEdit = () =>
     this.setState({
         edit: !this.state.edit
+    })
+
+    //function to toggle the houseImage larger or smaller, cursor also changes to plus
+    //or minus sign in App.css houseImage and houseImageBig
+    toggleDisplay = () =>
+    this.setState({
+        display: !this.state.display
     })
 
     render() {
@@ -39,11 +52,19 @@ export default class DisplayRental extends Component {
                    
                     <div>
                     <h1></h1>
-                    <img src={rental.imageUrl} alt="RentalPic" width="200" className="houseImage"/>
+                    {this.state.display?
+                    <span>
+                    <img src={rental.imageUrl} alt="RentalPic" width="300" onClick={this.toggleDisplay} className="houseImageBig"/>   
+                    </span>
+                    :
+                    <span>
+                    <img src={rental.imageUrl} alt="RentalPic" width="200" onClick={this.toggleDisplay} className="houseImage"/>
+                    </span>}
+                    
                     <div><span className="bold">Monthly Rent: </span>{formatter.format(rental.price)}</div>
                     <div><span className="bold">Bedrooms: </span> {rental.bd}</div>
                     <div><span className="bold">bathrooms: </span>{rental.bth}</div>
-                    <div><span className="bold">square feet: </span>{rental.sqfeet}</div>
+                    <div><span className="bold">square feet: </span>{formatNumber(rental.sqfeet)}</div>
                     <div><span className="bold">Street Address: </span>{rental.address}</div>
                     <div><span className="bold">Zip Code: </span>{rental.zip}</div>
                     </div>
